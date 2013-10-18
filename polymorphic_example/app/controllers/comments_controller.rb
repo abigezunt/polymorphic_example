768b9fdf0d3a72b19df782a416d9a95dcbab3485
Abby Howell <abby.g.howell@gmail.com>
2013-10-18 14:36:23 -0400

    working

--- polymorphic_example/app/controllers/comments_controller.rb
+++ polymorphic_example/app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
-    @comment = @commentable.comment.new
+    @comment = Comment.new
   end
 
   # GET /comments/1/edit
@@ -25,7 +25,7 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @commentable.comments.new(comment_params)
 
     respond_to do |format|
       if @comment.save
@@ -77,6 +77,6 @@ class CommentsController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :commentable_id)
     end
 end
